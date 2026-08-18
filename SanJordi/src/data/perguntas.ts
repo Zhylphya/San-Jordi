6c47b630ed6245bcc4532033ca66f8922f5f2911
Zhylphya <vitoriapixbet@gmail.com>
2026-08-17 23:31:33 -0300

    feat(data): create San Jordi´s questions

---
+++ SanJordi/src/data/perguntas.ts
@@ -0,0 +1,33 @@
+export const grupos = [
+    {
+        texto: 'Você aceita uma rosa de San Jordi?',
+        imagem: algumacoisa.png
+    },
+    {
+        texto: 'Tem certeza?\nO dragão pode aparecer...',
+        imagem: dragon.png
+    },
+    {
+        texto: 'Dragões não desistem tão fácil!',
+        imagem: Drag.png
+    },
+    {
+        texto: 'Cuidado...\nEle está chegando!',
+        imagem:
+    },
+    {
+        texto: 'última chance...\nRosas ou dragões?',
+        imagem:
+    },
+    {
+        texto: 'O dragão está fugindo de você',
+        imagem:
+    },
+    {
+        texto: 'Boa escolha!\nVocê ganhou uma rosa\n e uma história para chamar de sua!'
+    },
+    {
+        texto: 'Feliz San Jordi!'
+        imagem: 
+    }
+]
