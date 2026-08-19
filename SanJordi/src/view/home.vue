39d0cba377ad04e58dc47296ce227266182ad509
Zhylphya <vitoriapixbet@gmail.com>
2026-08-19 19:58:55 -0300

    feat(home): create buttons and define scale

--- SanJordi/src/view/home.vue
+++ SanJordi/src/view/home.vue
@@ -5,14 +5,26 @@ import { computed, ref } from 'vue'
 currentStage = ref(0)
 
 const question = computed(() => {
-    return questions[currentStage.value]
+    return questions[currentStage]
+})
+
+const buttonNoSize = computed(() => {
+    return 1 - (currentStage.value * 0.1)
+})
+
+const buttonYesSize = computed(() => {
+    return 1 + (currentStage.value * 0.1)
 })
 
 </script>
 
 <template>
-    
+
     <h1>{{ question.text }}</h1>
     <img :src="question.image">
+
+    <button @click="currentStage.value = questions.length -1" :style="{ transform: `scale(${buttonYesSize})` }"> Sim </button>
+    <button @click="currentStage.value++" :style="{ transform: `scale(${buttonNoSize})` }"> Não </button>
+
 
 </template>
