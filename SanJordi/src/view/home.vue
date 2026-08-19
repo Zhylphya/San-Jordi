943366176724e286de018c25d338ea6ac6b7786d
Zhylphya <vitoriapixbet@gmail.com>
2026-08-19 19:25:37 -0300

    feat(home): difine stages on home

---
+++ SanJordi/src/view/home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { questions } from './data/questions.ts'
+import { computed, ref } from 'vue'
+
+currentStage = ref(0)
+
+const question = computed(() => {
+    return questions[currentStage.value]
+})
+
+</script>
+
+<template>
+    
+    <h1>{{ question.text }}</h1>
+    <img :src="question.image">
+
+</template>
