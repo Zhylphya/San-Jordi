5b7dbf7254370beadaa0cf92d8fefff9b25afd10
Zhylphya <vitoriapixbet@gmail.com>
2026-08-23 14:13:36 -0300

    fix(SanJordi): fix all conflicts problem on project

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,40 +1,38 @@
-import Drag from '../asserts/floral-start.png'
+import DragonCastle from '../asserts/questionsImage/dragon-castle.png'
+import DragonSaintGeorge from '../asserts/questionsImage/dragon-saint-george.png'
+import Tail from '../asserts/questionsImage/tail-ornament.png'
+import Dragon from '../asserts/questionsImage/dragon.png'
+import RoseBook from '../asserts/questionsImage/rose-book-combined.png'
+import RoseTop from '../asserts/questionsImage/rose-book-top.png'
+import FinalBook from '../asserts/questionsImage/final-book.png'
 
 export const questions = [
     {
         text: 'Você aceita uma rosa de San Jordi?',
-        image: Drag
+        image: RoseTop
     },
     {
         text: 'Tem certeza? O dragão pode aparecer...',
-        image: Drag
+        image: Dragon
     },
     {
         text: 'Dragões não desistem tão fácil!',
-        image: Drag
+        image: DragonCastle
     },
     {
         text: 'Cuidado...Ele está chegando!',
-        image: Drag
+        image: DragonSaintGeorge
     },
     {
         text: 'última chance... Rosas ou dragões?',
-        image: Drag
-    },
-    {
-        text: 'O dragão está fugindo de você',
-        image: Drag
+        image: RoseBook
     },
         {
         text: 'Vou te dar uma chance porque não quero ver você sofrer...',
-        image: Drag
+        image: Tail
     },
     {
         text: 'Boa escolha! Você ganhou uma rosa e uma história para chamar de sua!',
-        image: Drag
+        image: FinalBook
     },
-    {
-        text: 'Feliz San Jordi!',
-        image: Drag
-    }
 ]
